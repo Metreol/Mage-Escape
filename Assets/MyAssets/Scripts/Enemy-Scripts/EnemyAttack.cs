874faa2aaf6a013608f85ea844b2ecde79f9d780
Metreol-XPS <project.metreol@gmail.com>
2023-12-27 19:06:43 +0000

    Fixed Wizard Emeny attack, now does damage with event in animation. - Had to duplicate the readonly animation to add event and register it to the damage method in EnemyAttack script.

--- Assets/MyAssets/Scripts/Enemy-Scripts/EnemyAttack.cs
+++ Assets/MyAssets/Scripts/Enemy-Scripts/EnemyAttack.cs
@@ -13,6 +13,13 @@ public class EnemyAttack : MonoBehaviour
         enemyAI = GetComponent<EnemyAI>();
     }
 
+    /* TODO : With Enemy-Wizard this doesn't work because the animation is READ-ONLY so I can't call the event
+     * as part of the animation.
+     * POTENTIAL SOLUTION: Give wizard attack much more range and add a projectile that does damage if it lands?
+     * - Will require a good bit of a rework but should be interesting!
+     * - Remember! This is still required for basic-enemy to work, keep this method for now at least or create 
+     * different classes for each case.
+     */
     public void AttackHitEvent()
     {
         Transform target = enemyAI.target;
